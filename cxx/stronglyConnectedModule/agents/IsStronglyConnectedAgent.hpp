0b0b515d8d240669a97f04281a2828978ef967e3
alexey_t <alexteleev682004@gmail.com>
2022-12-06 23:28:26 +0300

    first-commit

---
+++ cxx/stronglyConnectedModule/agents/IsStronglyConnectedAgent.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <sc-memory/kpm/sc_agent.hpp>
+
+#include "keynodes/keynodes.hpp"
+#include "IsStronglyConnectedAgent.generated.hpp"
+
+namespace exampleModule
+{
+    class IsStronglyConnectedAgent : public ScAgent
+    {
+        SC_CLASS(Agent, Event(Keynodes::question_is_strongly_connected, ScEvent::Type::AddOutputEdge))
+        SC_GENERATED_BODY()
+    };
+
+} // namespace exampleModule
