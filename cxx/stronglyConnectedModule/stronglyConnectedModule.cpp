f58fd6c207e657d4a5db1f75ae60e5e6a053e2e1
alexey_t <alexteleev682004@gmail.com>
2022-12-07 10:46:26 +0300

    change module_name

--- cxx/stronglyConnectedModule/stronglyConnectedModule.cpp
+++ cxx/stronglyConnectedModule/stronglyConnectedModule.cpp
@@ -4,7 +4,7 @@
 * (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
 */
 
-#include "exampleModule.hpp"
+#include "stronglyConnectedModule.hpp"
 #include "keynodes/keynodes.hpp"
 #include "agents/IsStronglyConnectedAgent.hpp"
 
